0e3607dbb0078dbb6673ce862fbda927ab876ae5
Gabriiel--Delazeri <gbldelazeridev@gmail.com>
2022-03-11 14:40:49 -0300

    Datatable add

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -14,7 +14,11 @@
 
     <!-- JQUERY -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.js"></script>
+    <script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.21/js/dataTables.bootstrap4.min.js"></script>
+
     <style>
         body{
             background-color: #97c2c0;
